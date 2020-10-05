7e8f198692e2319f77a9600bc6d5be1a37a6ef16
shub22mishra <47281314+shub22mishra@users.noreply.github.com>
2020-10-05 22:51:10 +0530

    Day 4 testing using chai and mocha

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
+  //   try {
+  //     throw new Error("Foo");
+  //     return res.send("Hello World!");
+  //   } catch (e) {
+  //     return res.status(500).send("Internal Server Error");
+  //   }
   return res.send("hello world!");
 });
 
-app.listen(4000, function () {
+app.listen(4000, () => {
   console.log("server running at port 4000");
 });
+module.exports = app;
